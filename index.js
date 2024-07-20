f50ab430b2b249df2c29b03c870ecc4df6ceb7df
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2024-07-20 12:08:41 +0530

    more projects and certificates slider added

--- index.js
+++ index.js
@@ -39,3 +39,38 @@ var typed = new Typed('#element', {
     cursorChar: '',
     loop: true
   });
+
+
+  let current = 1;
+  setInterval(() => {
+      let pdf1 = document.getElementById("pdf1");
+      let pdf2 = document.getElementById("pdf2");
+  
+      if (current === 1) {
+          pdf1.style.transition = "width 0.9s ease-in-out";
+          pdf1.style.width = "0%";
+          pdf2.style.transition = "width 0.9s ease-in-out";
+          pdf2.style.width = "100%";
+  
+          // setTimeout(() => {
+          //     pdf1.classList.add("hidden");
+          //     pdf2.classList.remove("hidden");
+          // }, 2000); // Apply the hidden class change after the transition completes
+  
+          current = 0;
+      } else {
+          pdf1.style.transition = "width 0.9s ease-in-out";
+          pdf1.style.width = "100%";
+          pdf2.style.transition = "width 0.9s ease-in-out";
+          pdf2.style.width = "0%";
+  
+          // setTimeout(() => {
+          //     pdf2.classList.add("hidden");
+          //     pdf1.classList.remove("hidden");
+          // }, 2000); // Apply the hidden class change after the transition completes
+  
+          current = 1;
+      }
+  }, 5000); // Increase the interval to allow transition to complete before switching
+  
+  
